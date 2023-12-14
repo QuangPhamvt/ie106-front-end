9efd8cdf1349aaf7fd98f97016f4142d53f7e4b4
QuangPM - VFA <quangpm220503vt@gmail.com>
2023-12-14 13:06:33 +0700

    feat: #16 add home view

--- src/view/Layout/HeaderLayout/components/SubNavHeaderComponent/index.tsx
+++ src/view/Layout/HeaderLayout/components/SubNavHeaderComponent/index.tsx
@@ -1,6 +1,6 @@
 export const SubNavHeaderComponent = () => {
   return (
-    <section className='flex flex-row items-center justify-center w-full py-4 space-x-12 text-[#4848A4] font-bold text-xl bg-white h-16'>
+    <section className='flex flex-row items-center justify-center w-full py-4 space-x-12 text-[#4848A4] font-medium text-xl bg-white h-16'>
       <div>Trinh Thám</div>
       <div>Tình cảm </div>
       <div>Kinh dị</div>
